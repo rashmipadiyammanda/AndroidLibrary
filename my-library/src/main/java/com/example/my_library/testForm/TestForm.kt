034d9a30917372fb4f8876fd6e4ec0f70709cda3
Rashmi <rashmipc@qliclabs.com>
2023-07-31 16:18:03 +0530

    Initial commit

---
+++ my-library/src/main/java/com/example/my_library/testForm/TestForm.kt
@@ -0,0 +1,11 @@
+package com.example.my_library.testForm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class testForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
