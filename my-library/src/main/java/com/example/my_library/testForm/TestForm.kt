094742c7219d0a5d7c9318f811900e2877f2abe8
Rashmi <rashmipc@qliclabs.com>
2023-07-31 17:23:53 +0530

    Done with new xml

--- my-library/src/main/java/com/example/my_library/testForm/TestForm.kt
+++ my-library/src/main/java/com/example/my_library/testForm/TestForm.kt
@@ -2,10 +2,11 @@ package com.example.my_library.testForm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.my_library.R
 
 class testForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activiy_test_form)
     }
 }
