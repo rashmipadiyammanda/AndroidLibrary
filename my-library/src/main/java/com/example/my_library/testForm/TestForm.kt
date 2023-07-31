acb9baba632251b920465bdd3c527714f557f7eb
Rashmi <rashmipc@qliclabs.com>
2023-07-31 17:43:47 +0530

    Done

--- my-library/src/main/java/com/example/my_library/testForm/TestForm.kt
+++ my-library/src/main/java/com/example/my_library/testForm/TestForm.kt
@@ -2,7 +2,7 @@ package com.example.my_library.testForm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.my_library.R
+import com.example.`my-library`.R
 
 class testForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
